a16c5657385b5eae918f592284e8ee095b7d841f
ideal_sea <ideal_sea@qq.com>
2019-10-15 21:01:24 +0800

    调整结构

--- src/Navigations/RootNavigation.js
+++ src/Navigations/RootNavigation.js
@@ -5,24 +5,39 @@ import {
     Route,
     Link
 } from "react-router-dom";
-import { Counter } from '../Section1/Index'
+import StateHook from '../StateHook/Index'
+import EffectHook from '../EffectHook/Index'
 
 export default function () {
     return (
         <Router>
-            <div>
+            <div className="App">
                 <nav>
                     <ul>
                         <li>
-                            <Link to="/">section1</Link>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/state-hook">State Hook</Link>
+                        </li>
+                        <li>
+                            <Link to="/effect-hook">Effect Hook</Link>
                         </li>
                     </ul>
                 </nav>
 
                 {/* A <Switch> looks through its children <Route>s and renders the first one that matches the current URL. */}
                 <Switch>
+                    <Route path="/state-hook">
+                        <StateHook />
+                        <p>state</p>
+                    </Route>
+                    <Route path="/effect-hook">
+                        <EffectHook></EffectHook>
+                        <p>effect</p>
+                    </Route>
                     <Route path="/">
-                        <Counter />
+                        <h1>Home Page</h1>
                     </Route>
                 </Switch>
             </div>
